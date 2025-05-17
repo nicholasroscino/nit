c13afb45b6aad34f11c7b6f5cdc992a7c65fdfbf
nicholas <nicholas.roscino@neulabs.com>
2025-05-17 14:57:53 +0200

    first draft for hash-object

---
+++ commands/hash-object.go
@@ -0,0 +1,49 @@
+package commands
+
+import (
+	"bytes"
+	"compress/gzip"
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"log"
+	"nit/utils"
+	"os"
+)
+
+func toGZip(content string) string {
+	var buf bytes.Buffer
+	zw := gzip.NewWriter(&buf)
+
+	_, err := zw.Write([]byte(content))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := zw.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	return buf.String()
+}
+
+func calculateBlobHeader(myString string) (string, string) {
+	header := fmt.Sprintf("blob %d\u0000", len(myString))
+	h := sha1.New()
+	io.WriteString(h, header+myString)
+	return fmt.Sprintf("%x\n", h.Sum(nil)), header
+}
+
+func HashObjectCommand(filePath string, projectBasePath string) {
+	fileContent, err := os.ReadFile(filePath)
+	utils.Check(err, "Unable to read file\n")
+
+	hash, header := calculateBlobHeader(string(fileContent))
+	gzipd := toGZip(header)
+
+	err = os.Mkdir(projectBasePath+"/nit/objects/"+hash[0:2], 0755)
+	utils.Check(err, "Unable to create object directory with hash:"+hash[0:2]+"\n")
+
+	err = os.WriteFile(projectBasePath+"/nit/objects/"+hash[0:2]+"/"+hash[2:], []byte(gzipd), 0644)
+	utils.Check(err, "Unable to write object file\n")
+}
